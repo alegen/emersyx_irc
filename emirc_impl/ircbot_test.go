e61030a4c7486299e5e3f862dcd41240251abc0d
alegen <alex@alegen.net>
2017-08-20 00:49:41 +0200

    ircbot test custom values

--- emirc_impl/ircbot_test.go
+++ emirc_impl/ircbot_test.go
@@ -1,23 +1,28 @@
 package main
 
 import(
+    "flag"
     "fmt"
     "testing"
     "time"
 )
+
+var nick *string = flag.String("nick", "", "IRC bot nick used during testing")
+var channel *string = flag.String("channel", "", "IRC channel to join during testing")
+var sendto *string = flag.String("sendto", "", "IRC user to send message to during testing")
 
 func TestConnection(t *testing.T) {
     // when running go test -short
     if testing.Short() {
         fmt.Println("TestConnection not executed with -short argument")
     } else {
-        bot := emirc.NewIRCBot("emersyx", "chat.freenode.net", 6667, false)
+        bot := NewIRCBot(*nick, "chat.freenode.net", 6667, false)
         err := bot.Connect()
         if err != nil {
             fmt.Println(err)
         } else {
-            bot.Join("#emersyx")
-            bot.Privmsg("alegen", "hey dad")
+            bot.Join(*channel)
+            bot.Privmsg(*sendto, "hello world!")
             time.Sleep(20 * time.Second)
         }
 
