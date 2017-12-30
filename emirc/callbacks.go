f4222f49fc22cd5f358045fd20fb4bffc46b47a9
Alexandru Geana <alex@alegen.net>
2017-12-30 17:06:57 +0100

    adding comments to callbacks.go

--- emirc/callbacks.go
+++ emirc/callbacks.go
@@ -6,6 +6,8 @@ import(
     irc "github.com/fluffle/goirc/client"
 )
 
+// This function converts a Line object received from the underlying IRC library into an emircapi.Message object
+// containing the same information.
 func newMessage(id string, line *irc.Line) emircapi.Message {
     var m emircapi.Message
 
@@ -19,6 +21,8 @@ func newMessage(id string, line *irc.Line) emircapi.Message {
     return m
 }
 
+// This function creates a callback for the underlying IRC library. The callback receives the Line object, converts it
+// into a emircapi.Message object and sends it via the IRCBot event channel.
 func channelCallback(bot *IRCBot) func(*irc.Conn, *irc.Line) {
     return func(conn *irc.Conn, line *irc.Line) {
         go func() {
@@ -27,12 +31,20 @@ func channelCallback(bot *IRCBot) func(*irc.Conn, *irc.Line) {
     }
 }
 
+// This function creates a callback for the underlying IRC library. The callback receives the Line object, converts it
+// into a emircapi.Message object and logs the contents.
 func loggingCallback(bot *IRCBot, logger *log.Logger) func(*irc.Conn, *irc.Line) {
     return func(conn *irc.Conn, line *irc.Line) {
         m := newMessage(bot.identifier, line)
-        logger.Printf("new message: %s\n", m.Command)
-        logger.Printf("raw:         %s\n", m.Raw)
-        logger.Printf("origin:      %s\n", m.Origin)
-        logger.Printf("parameters:  %s\n", m.Parameters)
+        logger.Printf("New message:\n")
+        logger.Printf("Source      %s\n", m.Source)
+        logger.Printf("Raw         %s\n", m.Raw)
+        logger.Printf("Command     %s\n", m.Command)
+        logger.Printf("Origin      %s\n", m.Origin)
+        logger.Printf("Parameters:\n")
+        for i, p := range m.Parameters {
+            logger.Printf("%d. %s\n", i, p)
+        }
+        logger.Printf("-----\n")
     }
 }
